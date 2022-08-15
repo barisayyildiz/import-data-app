2c020d3774b7954e3ee887ab5dff0e48bd0420f7
Barış <barisayyildiz.1799@gmail.com>
2022-08-15 17:05:10 +0300

    test(import-data-app): ImportPage, continue button initially disabled

--- src/tests/integration/integration.spec.js
+++ src/tests/integration/integration.spec.js
@@ -44,14 +44,6 @@ describe("Integration tests", () => {
     expect(screen.findByText("Select Forms")).toBeNull();
   });
 
-  // TODO: user redirected to '/import' when a form in selected and continue button pressed on modal
-
-  // TODO: modal gets opened when users clicks on 'change form' button
-
-  // TODO: file input component removed and file preview component added in page after file upload
-
-  // TODO: matching form component, form validation
-
   it("matching form component, form validation", async () => {
     const initialState = {
       form: {
@@ -95,11 +87,47 @@ describe("Integration tests", () => {
     expect(screen.getByTestId("file_preview")).toBeInTheDocument();
   });
 
+  it("continue button is initially disabled", () => {
+    const initialState = {
+      form: {
+        allForms: [
+          {
+            id: "#1",
+            title: "hazard detection",
+            updated_at: "2022-08-09 02:43:54",
+            count: "12",
+            url: "https://",
+          },
+          {
+            id: "#2",
+            title: "save transaction",
+            updated_at: "2022-08-09 02:43:54",
+            count: "12",
+            url: "https://",
+          },
+        ],
+        selectedFormId: "#1",
+      },
+    };
+
+    renderWithProviders(<ImportPage />, {
+      preloadedState: initialState,
+    });
+
+    expect(
+      screen.getByText("Continue").closest("button").disabled
+    ).toBeTruthy();
+  });
+
   // TODO: user redirected to '/success' page if form is valid
 
   // TODO: user redirected to '/' when BACK button is clicked before file upload
 
   // TODO: file removed when BACK button is clicked after file upload
 
-  // TODO: Continue button is initially disabled
+  // TODO: user redirected to '/import' when a form in selected and continue button pressed on modal
+
+  // TODO: modal gets opened when users clicks on 'change form' button
+
+  // TODO: file input component removed and file preview component added in page after file upload
 });
