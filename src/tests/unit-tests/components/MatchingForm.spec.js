fe25f12c48582351261087e13025396fff609860
Barış <barisayyildiz.1799@gmail.com>
2022-08-15 13:19:27 +0300

    test(import-data-app): MatchinForm unit tests

--- src/tests/unit-tests/components/MatchingForm.spec.js
+++ src/tests/unit-tests/components/MatchingForm.spec.js
@@ -1,23 +1,41 @@
-import { screen } from "@testing-library/react";
+import { fireEvent, screen } from "@testing-library/react";
+import MatchingForm from "../../../components/MatchingForm";
 import { renderWithProviders } from "../../helpers";
 
 describe("MatchingForm unit tests", () => {
-  it.todo("renders correct inputs");
-  // it("renders correct inputs", () => {
-  //   const importedColumns = [
-  //     {
-  //       value: "#1",
-  //       label: "name",
-  //     },
-  //     {
-  //       value: "#2",
-  //       label: "Your Adress",
-  //     },
-  //     {
-  //       value: "#3",
-  //       label: "Work",
-  //     },
-  //   ];
-  // });
-  // const formQuestions = ["Name", "Adress", "Work"];
+  let importedColumns = [];
+  let formQuestions = [];
+
+  beforeAll(() => {
+    importedColumns = [
+      {
+        value: "#1",
+        label: "name",
+      },
+      {
+        value: "#2",
+        label: "Your Adress",
+      },
+      {
+        value: "#3",
+        label: "Work",
+      },
+    ];
+    formQuestions = ["Name", "Adress", "Work"];
+    renderWithProviders(
+      <MatchingForm
+        onSubmit={jest.fn()}
+        importedColumns={importedColumns}
+        formQuestions={formQuestions}
+        errors={[]}
+      />
+    );
+  });
+
+  it("renders correct inputs", () => {
+    const inputs = document.querySelectorAll("select");
+    expect(inputs.length).toBe(formQuestions.length);
+  });
+
+  // TODO: continue button, validation control
 });
